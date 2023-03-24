5706f09f1ac6f7b9db97972b4d0937a193ff5817
Darien Alejandro Verdugo Reyna <L20303022@puertopenasco.tecnm.mx>
2023-03-24 15:14:57 -0700

    Stable

--- server/index.js
+++ server/index.js
@@ -1,26 +1,32 @@
-const app = require('./app');
-
-const socketIo = require('socket.io');
-const http = require('http');
-
 const { parser, puertocom } = require('./services/serial.js');
-let lecturas = '';
 
-app.listen(3000, () =>
-	console.log(`Dolphin app listening on port ${puertocom}!`)
-);
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
+
+server.listen(3000, () => {
+	console.log('listening on *:3000');
+});
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+	console.log('a user connected');
+});
 
 parser.on(
 	'data', //console.log,
 	function (data) {
 		console.log(data.toString());
+		io.emit('arduino:data', { value: data.toString() });
 	}
 );
 
 parser.on('err', function (err) {
 	console.log(err.message);
-});
-
-app.get('/', (req, res) => {
-	res.send('Hello skdksksmdl');
 });
--- server/services/serial.js
+++ server/services/serial.js
@@ -1,6 +1,6 @@
 const { SerialPort } = require('serialport');
 const { ReadlineParser } = require('@serialport/parser-readline');
-const port = new SerialPort({ path: 'COM8', baudRate: 9600 });
+const port = new SerialPort({ path: 'COM7', baudRate: 9600 });
 
 const parser = port.pipe(new ReadlineParser({ delimiter: '\r\n' }));
 
